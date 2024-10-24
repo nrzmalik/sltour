ab948e005744705891ea47b3d29543e6b720c0b9
nrzmalik <125977246+nrzmalik@users.noreply.github.com>
2024-10-24 21:37:27 +0500

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,39 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Y7EgxQaBXW":
+        Script1();
+        break;
+      case "6nkDWWkyzEQ":
+        Script2();
+        break;
+      case "5jqPb2ovYuL":
+        Script3();
+        break;
+      case "6CZi61m2to8":
+        Script4();
+        break;
+      case "6EaxUBTtcF8":
+        Script5();
+        break;
+      case "6RrZ4jT7HZE":
+        Script6();
+        break;
+      case "5qOy6KOLhm2":
+        Script7();
+        break;
+      case "5reAFTRkHFU":
+        Script8();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
